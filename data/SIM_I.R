8a58e492244921d5f33a2800c60a1c6051383382
ChihHaoWu1 <105507102+ChihHaoWu1@users.noreply.github.com>
2022-05-14 00:05:38 -0400

    Create SIM_I.R

---
+++ data/SIM_I.R
@@ -0,0 +1,11 @@
+# from https://github.com/XuegongLab/DenseFly4scRNAseq/
+
+library(splatter)
+library(scater)
+params <-newSplatParams()
+params<-setParams(params,update=list(nGenes=10000,batchCells=2000,de.prob=0.08))
+sim <- splatSimulate(params,group.prob=c(0.3,0.3,0.3,0.05,0.05),method="groups",verbose = FALSE)
+data<-counts(sim)
+data <-t(data)
+label<-colData(sim)[3]
+write.table(cbind(data,label),"SelfMapping.txt")
